a49d8249fc2caa4c089a8f4987a9561da88f975e
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2024-01-10 20:41:19 -0700

    trying to get css to take

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const static = require("./routes/static")
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
 app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
