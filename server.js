dc6f320e8c936a735c68f6219fd34b21fecfd325
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2024-01-25 17:48:57 -0700

    error handling

--- server.js
+++ server.js
@@ -30,6 +30,24 @@ app.use(static)
 app.get("/", baseController.buildHome)
 // Inventory Routes
 app.use("/inv", inventoryRoute)
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have gotten lost.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+************************** */
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 
 /* ***********************
